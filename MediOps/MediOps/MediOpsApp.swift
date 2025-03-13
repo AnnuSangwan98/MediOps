01ddb7481fa0da55ade0ec4d6f298aec29158895
AnnuSangwan98 <annusangwan098@gmail.com>
2025-03-13 18:21:30 +0530

    Create MediOps project

---
+++ MediOps/MediOps/MediOpsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MediOpsApp.swift
+//  MediOps
+//
+//  Created by Abcom on 13/03/25.
+//
+
+import SwiftUI
+
+@main
+struct MediOpsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
